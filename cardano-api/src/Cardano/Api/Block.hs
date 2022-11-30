a53c16eae8d236619adf155d43f8522304f9ac0b
Sebastian Nagel <sebastian.nagel@ncoding.at>
2022-11-30 10:38:05 +0100

    Add ToJSON/FromJSON instances for ChainPoint
    There is already a ToJSON instance for ChainTip so we figured we could
    upstream this from hydra-cardano-api.

--- cardano-api/src/Cardano/Api/Block.hs
+++ cardano-api/src/Cardano/Api/Block.hs
@@ -342,6 +342,24 @@ data ChainPoint = ChainPointAtGenesis
   deriving (Eq, Show)
 
 
+instance ToJSON ChainPoint where
+  toJSON = \case
+    ChainPointAtGenesis -> object ["tag" .= String "ChainPointAtGenesis"]
+    ChainPoint slot blockHash ->
+      object
+        [ "tag" .= String "ChainPoint"
+        , "slot" .= toJSON slot
+        , "blockHash" .= toJSON blockHash
+        ]
+
+instance FromJSON ChainPoint where
+  parseJSON = withObject "ChainPoint" $ \o -> do
+    tag <- o .: "tag"
+    case tag :: Text of
+      "ChainPointAtGenesis" -> pure ChainPointAtGenesis
+      "ChainPoint" -> ChainPoint <$> o .: "slot" <*> o .: "blockHash"
+      _ -> fail "Expected tag to be ChainPointAtGenesis | ChainPoint"
+
 toConsensusPointInMode :: ConsensusMode mode
                        -> ChainPoint
                        -> Consensus.Point (ConsensusBlockForMode mode)
